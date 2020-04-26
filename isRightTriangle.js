b72666fafbbe9f0ade24ca1c0cadd6fc42cbc439
Gregory Wickham <gwickham99@gmail.com>
2020-04-26 13:33:44 -0400

    triangle?

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -2,7 +2,9 @@ const isRightTriangle = (a, b, c) => {
   if (a == b && b == c) {
     return false;
   }
-  return a ** 2 + b ** 2 == c ** 2;
+  const s = [a, b, c].sort();
+
+  return s[0] ** 2 + s[1] ** 2 == s[2] ** 2;
 };
 
 module.exports = isRightTriangle;
